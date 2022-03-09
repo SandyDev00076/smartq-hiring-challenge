e50005d5dd435dd79421d64ad86afa0669adc416
Sanjeet Tiwari <sanjeettiwari00076@gmail.com>
2022-03-10 00:52:10 +0530

    add types for category and item

---
+++ src/types/Category.ts
@@ -0,0 +1,5 @@
+export interface Category {
+  name: string;
+  banner: string;
+  icon: string;
+}
---
+++ src/types/Item.ts
@@ -0,0 +1,9 @@
+export interface Item {
+  category: string;
+  fooddescription: string;
+  foodid: string;
+  foodname: string;
+  imageurl: string;
+  price: number;
+  sessionlist: Array<string>;
+}
